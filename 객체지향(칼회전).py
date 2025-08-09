5a7629e4929608df0348823b0cdc61df9998b1e5
Leehoseob93 <dlghtjq213@naver.com>
2025-08-09 15:07:19 +0900

    투사체 추가

--- 객체지향(칼회전).py
+++ 객체지향(칼회전).py
@@ -83,23 +83,36 @@ class Knife:
         self.knife_left = QPixmap('knife_left.png')
         self.y = character.y + character.height()//3
         self.direction = character.direction
+        self.angle = 0
+        self.rotation_speed = 25
 
         if self.direction == "right":
             self.x = character.x + character.width()//2
             self.k_speed = 15
+            self.rotation_speed = 50
         elif self.direction == "left":
             self.x = character.x - character.width()//2
             self.k_speed = -15
+            self.rotattion_speed = -50
 
     def move(self):
         self.x += self.k_speed
+        self.angle += self.rotation_speed
 
     def draw(self,painter):
         if self.direction == "right":
-            painter.drawPixmap(self.x, self.y, self.knife_right)
+            pixmap = self.knife_right
         elif self.direction == "left":
-            painter.drawPixmap(self.x, self.y, self.knife_left)
+            pixmap = self.knife_left
 
+        center_x = pixmap.width()//2
+        center_y = pixmap.height()//2
+
+        painter.save()
+        painter.translate(self.x + center_x, self.y + center_y)
+        painter.rotate(self.angle)
+        painter.drawPixmap(-center_x, -center_y, pixmap)
+        painter.restore()
     
 class CharacterManager:
     def __init__(self, character:Character):
